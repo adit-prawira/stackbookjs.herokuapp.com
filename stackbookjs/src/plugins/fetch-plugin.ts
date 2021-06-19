4c0f3ff3bf951b120e95476f9c7dd908cadfd508
Aditya Prawira <adityaprawira96@gmail.com>
2021-06-19 19:11:52 +1000

    refactor fetch-plugin.ts file andded functionality to import css files

--- stackbookjs/src/plugins/fetch-plugin.ts
+++ stackbookjs/src/plugins/fetch-plugin.ts
@@ -10,23 +10,55 @@ export const fetchPlugin = (inputCode: string) => {
     return {
         name: "fetch-plugin",
         setup(build: esbuild.PluginBuild) {
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
-                //console.log("onLoad", args);
-                if (args.path === "index.js") {
-                    return {
-                        loader: "jsx",
-                        contents: inputCode,
-                    };
-                }
+            // load for file name exactly named "index.js"
+            build.onLoad({ filter: /(^index\.js$)/ }, () => {
+                return {
+                    loader: "jsx",
+                    contents: inputCode,
+                };
+            });
 
-                // Check if the modules or file has been stored or already in cache
-                // if yes return early, otherwise store the response
+            // On load code block that check if the imported modules or file has
+            // been stored or already in cache if yes return early, otherwise store the response
+            build.onLoad({ filter: /.*/ }, async (args: any) => {
                 const cachedResult =
                     await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
                 if (cachedResult) return cachedResult;
+            });
 
-                // args.path is now https://unpkg.com/tiny-test-pkg@1.0.0/index.js
+            // load for any file that ends with .css, basically any css file
+            build.onLoad({ filter: /.css$/ }, async (args: any) => {
+                // args.path is now https://unpkg.com/.../...css
+                const { data, request } = await axios.get(args.path);
+
+                // replacing all newline character in the data with empty string
+                // then escape any double quotes and single quotes
+                const escaped = data
+                    .replace(/\n/g, "")
+                    .replace(/"/g, '\\"')
+                    .replace(/'/g, "\\'");
+
+                // If not working with css import return the original data, otherwise
+                // return the following Javascript commands.
+                const contents = `
+                    const style = document.createElement("style");
+                    style.innerText = '${escaped}';
+                    document.head.appendChild(style);
+                    `;
+
+                // storing file or module in cache
+                const result: esbuild.OnLoadResult = {
+                    loader: "jsx",
+                    contents,
+                    resolveDir: new URL("./", request.responseURL).pathname,
+                };
+                await fileCache.setItem(args.path, result);
+                return result;
+            });
+
+            build.onLoad({ filter: /.*/ }, async (args: any) => {
+                // args.path is now https://unpkg.com/.../...js
                 const { data, request } = await axios.get(args.path);
 
                 // storing file or module in cache
