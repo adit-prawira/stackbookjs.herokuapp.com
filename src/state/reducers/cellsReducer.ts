fb1fb20281d77b7a751c236ba312db138a2275ab
Aditya Prawira <adityaprawira96@gmail.com>
2021-07-09 17:54:05 +1000

    finishing cells actions and reducers and also do some manual testing to see redux's performances on store state of cells

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -19,13 +19,17 @@ const initialState: CellsState = {
     data: {},
 };
 
+function randomId() {
+    return Math.random().toString(36).substr(2, 5);
+}
+
 const cellsReducer = produce(
-    (state: CellsState = initialState, action: Action): void => {
+    (state: CellsState = initialState, action: Action): CellsState => {
         switch (action.type) {
             case ActionTypes.DELETE_CELL:
                 delete state.data[action.payload];
                 state.order.filter((id) => id !== action.payload);
-                return;
+                return state;
             case ActionTypes.MOVE_CELL:
                 const { direction } = action.payload;
                 const i = state.order.findIndex(
@@ -34,19 +38,33 @@ const cellsReducer = produce(
                 const nCells = state.order.length - 1;
                 const targetIndex = direction === "up" ? i - 1 : i + 1;
                 if (targetIndex < 0 || targetIndex > nCells) {
-                    return;
+                    return state;
                 }
                 state.order[i] = state.order[targetIndex];
                 state.order[targetIndex] = action.payload.id;
-                return;
+                return state;
             case ActionTypes.INSERT_CELL_BEFORE:
-                return;
+                const cell: Cell = {
+                    content: "",
+                    type: action.payload.type,
+                    id: randomId(),
+                };
+                state.data[cell.id] = cell; // add new cell to the state data
+                const indexBefore = state.order.findIndex(
+                    (id) => id === action.payload.id
+                );
+                if (indexBefore < 0) {
+                    state.order.push(cell.id);
+                    return state;
+                }
+                state.order.splice(indexBefore, 0, cell.id);
+                return state;
             case ActionTypes.UPDATE_CELL: // updating the content of the cell
                 const { id, content } = action.payload;
                 state.data[id].content = content;
-                return;
+                return state;
             default:
-                return;
+                return state;
         }
     }
 );
