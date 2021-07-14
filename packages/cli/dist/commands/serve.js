03c760aec067e7ec6179dc337be94fd337f620a1
Aditya Prawira <adityaprawira96@gmail.com>
2021-07-14 15:58:58 +1000

    Added setup for cli commander package

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
 var commander_1 = require("commander");
 exports.serveCommand = new commander_1.Command()
-    .command("serve")
+    .command("serve [filename]")
     .description("Opening a file for editing")
+    .option("-p, --port <number>", "Port number to run the server on")
     .action(function () {
     console.log("Ready to serve a file");
 });
--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,7 +1,8 @@
 import { Command } from "commander";
 export const serveCommand = new Command()
-    .command("serve")
+    .command("serve [filename]")
     .description("Opening a file for editing")
+    .option("-p, --port <number>", "Port number to run the server on", " ")
     .action(() => {
         console.log("Ready to serve a file");
     });
